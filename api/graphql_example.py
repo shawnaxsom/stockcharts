db04bfb1b44d5d58768453ddf2f85a89aef93ff8
Shawn Axsom <shawn.axsom@ambyint.com>
2018-02-19 22:50:40 -0500

    Starting playing with a GraphQL endpoint for getting chart data

---
+++ api/graphql_example.py
@@ -0,0 +1,23 @@
+import graphene
+import json
+import requests
+
+class Query(graphene.ObjectType):
+    peers = graphene.String(symbol=graphene.String())
+    chart = graphene.String(symbol=graphene.String(), timespan=graphene.String())
+
+    def resolve_peers(self, info, symbol):
+        return json.loads(requests.get('https://api.iextrading.com/1.0/stock/' + symbol + '/peers').content)
+
+    def resolve_chart(self, info, symbol, timespan):
+        return json.loads(requests.get('https://api.iextrading.com/1.0/stock/' + symbol + '/chart/' + timespan).content)
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute('''
+    {
+      chart(symbol: "msft", timespan: "5y")
+      peers(symbol: "msft")
+    }
+    ''')
+print(result.data)
